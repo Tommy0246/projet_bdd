ef634de278e579f73180895e990a0009dc82163a
Tommy0246 <120111988+Tommy0246@users.noreply.github.com>
2023-02-09 16:03:11 +0100

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,198 @@
+INSERT INTO personne VALUES ('Chabas', 'Tom', 19);
+INSERT INTO personne VALUES ('Dupont', 'Jacques', 35);
+INSERT INTO personne VALUES ('Martin', 'Emilie', 25);
+INSERT INTO personne VALUES ('Durand', 'Marc', 45);
+INSERT INTO personne VALUES ('Petit', 'Sophie', 30);
+INSERT INTO personne VALUES ('Moreau', 'Antoine', 50);
+INSERT INTO personne VALUES ('Obama', 'Barack', 60);
+INSERT INTO personne VALUES ('Winston', 'Churchill', 70);
+INSERT INTO personne VALUES ('Gandhi', 'Mahatma', 80);
+INSERT INTO personne VALUES ('Curie', 'Marie', 45);
+INSERT INTO personne VALUES ('Einstein', 'Albert', 50);
+INSERT INTO personne VALUES ('Ronaldo', 'Cristiano', 36);
+INSERT INTO personne VALUES ('Messi', 'Lionel', 34);
+INSERT INTO personne VALUES ('Neymar', 'Jr', 29);
+INSERT INTO personne VALUES ('Mbappé', 'Kylian', 22);
+INSERT INTO personne VALUES ('Rooney', 'Wayne', 37);
+INSERT INTO personne VALUES ('Jennie', 'Kim', 25);
+INSERT INTO personne VALUES ('Jisoo', 'Park', 26);
+INSERT INTO personne VALUES ('Lisa', 'Manoban', 25);
+INSERT INTO personne VALUES ('Rose', 'Park', 27);
+INSERT INTO personne VALUES ('Momo', 'Hirai', 24);
+INSERT INTO personne VALUES ('Bocuse', 'Paul', 90);
+INSERT INTO personne VALUES ('Robuchon', 'Joël', 73);
+INSERT INTO personne VALUES ('Adrià', 'Ferran', 56);
+INSERT INTO personne VALUES ('Pic', 'Anne-Sophie', 50);
+INSERT INTO personne VALUES ('Lefebvre', 'Yannick', 48);
+INSERT INTO personne VALUES ('Etchebest', 'Philippe', 41);
+INSERT INTO personne VALUES ('Strummer', 'Joe', 60);
+INSERT INTO personne VALUES ('Pop', 'Iggy', 72);
+INSERT INTO personne VALUES ('Ramone', 'Joey', 55);
+INSERT INTO personne VALUES ('Coke', 'Karen', 50);
+INSERT INTO personne VALUES ('Case', 'Exene', 68);
+INSERT INTO personne VALUES ('Skywalker', 'Luke', 45);
+INSERT INTO personne VALUES ('Solo', 'Han', 35);
+INSERT INTO personne VALUES ('Organa', 'Leia', 40);
+INSERT INTO personne VALUES ('Kenobi', 'Obi-Wan', 60);
+INSERT INTO personne VALUES ('Vader', 'Darth', 45);
+INSERT INTO personne VALUES ('Nyla', 'Ravi', 25);
+INSERT INTO personne VALUES ('Gavry', 'Elen', 32);
+INSERT INTO personne VALUES ('Larko', 'Mira', 37);
+INSERT INTO personne VALUES ('Kelvi', 'Nina', 41);
+INSERT INTO personne VALUES ('Soren', 'Lya', 30);
+
+INSERT INTO etat_habitation VALUES (1, 'neuf');
+INSERT INTO etat_habitation VALUES (2, 'très bon état');
+INSERT INTO etat_habitation VALUES (3, 'bon état');
+INSERT INTO etat_habitation VALUES (4, 'à rénover');
+INSERT INTO etat_habitation VALUES (5, 'détruit');
+
+INSERT INTO Logement VALUES (1, '41 boulevard des rois', 'maison', 4, 130, 'vente', '2020-01-04', 'Paris', 1, 500000, 3, 'Winston');
+INSERT INTO Logement VALUES (2, '25 rue de la paix', 'appartement', 2, 70, 'location', '2022-03-02', 'Lyon', 0, 1000, 5, 'Petit');
+INSERT INTO Logement VALUES (3, '120 avenue des tulipes', 'maison', 3, 100, 'vente', '2021-05-01', 'Marseille', 1, 400000, 2, 'Obama');
+INSERT INTO Logement VALUES (4, '78 rue de la mer', 'appartement', 1, 50, 'location', '2021-12-01', 'Nice', 1, 800, 1, 'Dupont');
+INSERT INTO Logement VALUES (5, '64 avenue des fleurs', 'maison', 5, 150, 'vente', '2022-02-01', 'Bordeaux', 0, 600000, 4, 'Chabas');
+INSERT INTO Logement VALUES (6, '32 rue des montagnes', 'appartement', 3, 90, 'location', '2021-09-01', 'Grenoble', 1, 1300, 5, 'Vader');
+INSERT INTO Logement VALUES (7, '85 boulevard des cerisiers', 'maison', 6, 200, 'vente', '2022-01-01', 'Toulouse', 0, 800000, 3, 'Moreau');
+INSERT INTO Logement VALUES (8, '47 rue des champs', 'appartement', 2, 60, 'location', '2021-07-01', 'Strasbourg', 1, 900, 2, 'Durand');
+INSERT INTO Logement VALUES (9, '51 avenue des roses', 'maison', 4, 140, 'vente', '2021-06-01', 'Nantes', 0, 700000, 1, 'Petit');
+INSERT INTO Logement VALUES (10, '99 rue des arbres', 'appartement', 1, 45, 'location', '2022-05-01', 'Lille', 1, 700, 5, 'Coke');
+INSERT INTO Logement VALUES (11, '12 rue de la tour Eiffel', 'appartement', 3, 70, 'location', '2022-05-02', 'Paris', 1, 1500, 4, 'Petit');
+INSERT INTO Logement VALUES (12, '5 avenue des Champs Elysées', 'appartement', 2, 60, 'vente', '2021-12-01', 'Paris', 0, 900000, 5, 'Obama');
+INSERT INTO Logement VALUES (13, '2 boulevard de la Villette', 'maison', 5, 140, 'vente', '2022-03-06', 'Lyon', 1, 800000, 2, 'Case');
+INSERT INTO Logement VALUES (14, '18 rue de la paix', 'appartement', 1, 30, 'location', '2022-01-01', 'Lyon', 0, 500, 1, 'Moreau');
+INSERT INTO Logement VALUES (15, '9 avenue des Gobelins', 'maison', 6, 200, 'vente', '2021-09-15', 'Marseille', 1, 1000000, 5, 'Einstein');
+INSERT INTO Logement VALUES (16, '7 place des Terreaux', 'appartement', 2, 50, 'location', '2022-07-01', 'Marseille', 0, 700, 3, 'Dupont');
+INSERT INTO Logement VALUES (17, '3 boulevard des Belges', 'maison', 4, 120, 'vente', '2021-11-01', 'Bordeaux', 1, 500000, 4, 'Etchebest');
+INSERT INTO Logement VALUES (18, '1 avenue Thiers', 'appartement', 1, 40, 'location', '2022-02-01', 'Bordeaux', 0, 600, 2, 'Curie');
+INSERT INTO Logement VALUES (19, '5 rue des Arts', 'maison', 5, 150, 'vente', '2021-08-01', 'Toulouse', 1, 700000, 5, 'Ronaldo');
+INSERT INTO Logement VALUES (20, '9 rue de la Daurade', 'appartement', 2, 55, 'location', '2022-04-01', 'Toulouse', 0, 800, 3, 'Neymar');
+INSERT INTO Logement VALUES (21, '12 boulevard de la Marne', 'maison', 4, 130, 'vente', '2021-12-01', 'Lille', 1, 600000, 4, 'Rooney');
+INSERT INTO Logement VALUES (22, '12 rue des lilas', 'maison', 3, 90, 'location', '2022-07-01', 'Lyon', 1, 80000, 4, 'Petit');
+INSERT INTO Logement VALUES (23, '5 avenue des cerisiers', 'appartement', 2, 60, 'vente', '2022-06-15', 'Marseille', 1, 150000, 3, 'Martin');
+INSERT INTO Logement VALUES (24, '7 rue de la montagne', 'maison', 4, 120, 'location', '2022-05-20', 'Nice', 1, 100000, 5, 'Kenobi');
+INSERT INTO Logement VALUES (25, '19 boulevard des roses', 'appartement', 1, 40, 'vente', '2022-04-10', 'Toulouse', 0, 50, 2, 'Durand');
+INSERT INTO Logement VALUES (26, '3 rue des étoiles', 'maison', 5, 150, 'location', '2022-03-01', 'Bordeaux', 1, 150000, 5, 'Einstein');
+INSERT INTO Logement VALUES (27, '9 avenue des pommiers', 'appartement', 2, 70, 'vente', '2022-02-15', 'Lille', 1, 200000, 4, 'Obama');
+INSERT INTO Logement VALUES (28, '11 rue des champs', 'maison', 4, 110, 'location', '2022-01-10', 'Nantes', 0, 900, 3, 'Winston');
+INSERT INTO Logement VALUES (29, '21 boulevard des violettes', 'appartement', 1, 30, 'vente', '2021-12-20', 'Strasbourg', 1, 100000, 2, 'Gandhi');
+INSERT INTO Logement VALUES (30, '17 rue des tournesols', 'maison', 5, 140, 'location', '2021-11-01', 'Montpellier', 1, 1300, 5, 'Curie');
+INSERT INTO Logement VALUES (31, '2 avenue des fleurs', 'appartement', 2, 50, 'vente', '2021-10-15', 'Rennes', 0, 1700, 4, 'Chabas');
+INSERT INTO Logement VALUES (32, '12 avenue des fleurs', 'appartement', 2, 50, 'location', '2022-07-01', 'Lyon', 1, 900, 3, 'Chabas');
+INSERT INTO Logement VALUES (33, '54 rue des jardins', 'maison', 5, 180, 'vente', '2022-08-15', 'Marseille', 1, 40000, 4, 'Dupont');
+INSERT INTO Logement VALUES (34, '72 avenue du soleil', 'appartement', 1, 30, 'location', '2022-09-01', 'Nice', 1, 550, 2, 'Rose');
+INSERT INTO Logement VALUES (35, '86 boulevard de la mer', 'maison', 4, 140, 'vente', '2022-10-01', 'Bordeaux', 1, 35000, 5, 'Lisa');
+INSERT INTO Logement VALUES (36, '98 rue des champs', 'appartement', 3, 70, 'location', '2022-11-01', 'Toulouse', 1, 1200, 4, 'Petit');
+INSERT INTO Logement VALUES (37, '45 rue des montagnes', 'maison', 6, 200, 'vente', '2022-12-01', 'Grenoble', 1, 55000, 3, 'Moreau');
+INSERT INTO Logement VALUES (38, '67 avenue des arbres', 'appartement', 2, 50, 'location', '2023-01-01', 'Lille', 1, 700, 2, 'Obama');
+INSERT INTO Logement VALUES (39, '89 boulevard des collines', 'maison', 5, 180, 'vente', '2023-02-01', 'Nantes', 1, 45000000, 5, 'Jennie');
+INSERT INTO Logement VALUES (40, '11 rue des forêts', 'appartement', 1, 30, 'location', '2023-03-01', 'Rennes', 1, 600, 3, 'Gandhi');
+INSERT INTO Logement VALUES (41, '33 avenue des lacs', 'maison', 4, 140, 'vente', '2023-04-01', 'Strasbourg', 1, 32000, 4, 'Jisoo');
+
+INSERT INTO Garage(ID_garage, nom, surface, ID_logement)
+VALUES
+(1, 'Garage 1', 50, 1),
+(2, 'Garage 2', 60, 2),
+(3, 'Garage 3', 70, 3),
+(4, 'Garage 4', 80, 4),
+(5, 'Garage 5', 90, 5),
+(6, 'Garage 6', 100, 6),
+(7, 'Garage 7', 110, 7),
+(8, 'Garage 8', 120, 8),
+(9, 'Garage 9', 130, 9),
+(10, 'Garage 10', 140, 10),
+(11, 'Garage 11', 150, 11),
+(12, 'Garage 12', 160, 12),
+(13, 'Garage 13', 170, 13),
+(14, 'Garage 14', 180, 14),
+(15, 'Garage 15', 190, 15),
+(16, 'Garage 16', 200, 16),
+(17, 'Garage 17', 210, 17),
+(18, 'Garage 18', 220, 18),
+(19, 'Garage 19', 230, 19),
+(20, 'Garage 20', 240, 20),
+(21, 'Garage 21', 250, 21),
+(22, 'Garage 22', 260, 22),
+(23, 'Garage 23', 270, 23),
+(24, 'Garage 24', 280, 24),
+(25, 'Garage 25', 290, 25),
+(26, 'Garage 26', 300, 26),
+(27, 'Garage 27', 310, 27),
+(28, 'Garage 28', 320, 28),
+(29, 'Garage 29', 330, 29),
+(30, 'Garage 30', 340, 30),
+(31, 'Garage 31', 350, 31),
+(32, 'Garage 32', 360, 32),
+(33, 'Garage 33', 370, 33),
+(34, 'Garage 34', 380, 34),
+(35, 'Garage 35', 390, 35),
+(36, 'Garage 36', 400, 36),
+(37, 'Garage 37', 410, 37),
+(38, 'Garage 38', 420, 38),
+(39, 'Garage 39', 430, 39),
+(40, 'Garage 40', 440, 40);
+
+INSERT INTO Agence VALUES (1, 'StephanePlazat');
+
+INSERT INTO transac VALUES (1, 1000+25000, 'location', 1, 1, 'Chabas');
+INSERT INTO transac VALUES (2, 1000+20000, 'vente', 1, 3, 'Messi');
+INSERT INTO transac VALUES (3, 1000+20000, 'location', 1, 4, 'Einstein');
+INSERT INTO transac VALUES (4, 1000+30000, 'vente', 1, 5, 'Ronaldo');
+INSERT INTO transac VALUES (5, 1000+27000, 'location', 1, 6, 'Neymar');
+INSERT INTO transac VALUES (6, 1000+24000, 'vente', 1, 7, 'Rooney');
+INSERT INTO transac VALUES (7, 1000+29000, 'location', 1, 8, 'Jennie');
+INSERT INTO transac VALUES (8, 1000+26000, 'vente', 1, 9, 'Jisoo');
+INSERT INTO transac VALUES (9, 1000+25500, 'location', 1, 10, 'Lisa');
+INSERT INTO transac VALUES (10, 1000+27500, 'vente', 1, 11, 'Rose');
+INSERT INTO transac VALUES (11, 1000+31000, 'location', 1, 12, 'Momo');
+INSERT INTO transac VALUES (12, 1000+28000, 'vente', 1, 13, 'Bocuse');
+INSERT INTO transac VALUES (13, 1000+29500, 'location', 1, 14, 'Robuchon');
+INSERT INTO transac VALUES (14, 1000+33000, 'vente', 1, 15, 'Adrià');
+INSERT INTO transac VALUES (15, 1000+30000, 'location', 1, 16, 'Pic');
+INSERT INTO transac VALUES (16, 1000+26500, 'vente', 1, 17, 'Lefebvre');
+INSERT INTO transac VALUES (17, 1000+27000, 'location', 1, 18, 'Etchebest');
+INSERT INTO transac VALUES (18, 1000+32000, 'vente', 1, 19, 'Strummer');
+INSERT INTO transac VALUES (19, 1000+33500, 'location', 1, 20, 'Pop');
+INSERT INTO transac VALUES (20, 1000+31000, 'vente', 1, 21, 'Ramone');
+INSERT INTO transac VALUES (21, 1000+30500, 'location', 1, 22, 'Coke');
+INSERT INTO transac VALUES (22, 1000+29000, 'vente', 1, 23, 'Case');
+INSERT INTO transac VALUES (23, 1000+29500, 'location', 1, 24, 'Skywalker');
+INSERT INTO transac VALUES (24, 1000+28000, 'vente', 1, 25, 'Solo');
+INSERT INTO transac VALUES (25, 1000+31500, 'location', 1, 26, 'Organa');
+INSERT INTO transac VALUES (26, 1000+30000, 'vente', 1, 27, 'Vader');
+INSERT INTO transac VALUES (27, 1000+34000, 'location', 1, 28, 'Nyla');
+INSERT INTO transac VALUES (28, 1000+33000, 'vente', 1, 29, 'Gavry');
+INSERT INTO transac VALUES (29, 1000+31000, 'location', 1, 30, 'Larko');
+INSERT INTO transac VALUES (30, 1000+32000, 'vente', 1, 31, 'Kelvi');
+INSERT INTO transac VALUES (31, 1000+29000, 'location', 1, 32, 'Soren');
+INSERT INTO transac VALUES (32, 1000+30000, 'vente', 1, 33, 'Vader');
+
+INSERT INTO Visite VALUES (1, '2023-03-02', 'Dupont', 2, 1);
+INSERT INTO Visite VALUES (2, '2023-03-03', 'Martin', 3, 1);
+INSERT INTO Visite VALUES (3, '2023-03-04', 'Durand', 4, 1);
+INSERT INTO Visite VALUES (4, '2023-03-05', 'Petit', 5, 1);
+INSERT INTO Visite VALUES (5, '2023-03-06', 'Moreau', 6, 1);
+INSERT INTO Visite VALUES (6, '2023-03-07', 'Obama', 7, 1);
+INSERT INTO Visite VALUES (7, '2023-03-08', 'Winston', 8, 1);
+INSERT INTO Visite VALUES (8, '2023-03-09', 'Gandhi', 9, 1);
+INSERT INTO Visite VALUES (9, '2023-03-10', 'Curie', 10, 1);
+INSERT INTO Visite VALUES (10, '2023-03-11', 'Einstein', 11, 1);
+INSERT INTO Visite VALUES (11, '2023-03-12', 'Ronaldo', 12, 1);
+INSERT INTO Visite VALUES (12, '2023-03-13', 'Messi', 13, 1);
+INSERT INTO Visite VALUES (13, '2023-03-14', 'Neymar', 14, 1);
+INSERT INTO Visite VALUES (14, '2023-03-15', 'Mbappé', 15, 1);
+INSERT INTO Visite VALUES (15, '2023-03-16', 'Rooney', 16, 1);
+INSERT INTO Visite VALUES (16, '2023-03-17', 'Jennie', 17, 1);
+INSERT INTO Visite VALUES (17, '2023-03-18', 'Jisoo', 18, 1);
+INSERT INTO Visite VALUES (18, '2023-03-19', 'Lisa', 19, 1);
+INSERT INTO Visite VALUES (19, '2023-03-20', 'Rose', 20, 1);
+INSERT INTO Visite VALUES (20, '2023-03-21', 'Momo', 21, 1);
+INSERT INTO Visite VALUES (21, '2023-03-22', 'Bocuse', 22, 1);
+INSERT INTO Visite VALUES (22, '2023-03-23', 'Robuchon', 23, 1);
+INSERT INTO Visite VALUES (23, '2023-03-24', 'Adrià', 24, 1);
+INSERT INTO Visite VALUES (24, '2023-03-25', 'Pic', 25, 1);
+INSERT INTO Visite VALUES (25, '2023-03-26', 'Lefebvre', 26, 1);
+INSERT INTO Visite VALUES (26, '2023-03-27', 'Etchebest', 27, 1);
+INSERT INTO Visite VALUES (27, '2023-03-28', 'Strummer', 28, 1);
+INSERT INTO Visite VALUES (28, '2023-03-29', 'Pop', 29, 1);
+INSERT INTO Visite VALUES (29, '2023-03-30', 'Ramone', 30, 1);
---
+++ tables.sql
@@ -0,0 +1,70 @@
+CREATE TABLE Agence(
+   ID_agence INT,
+   nom VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_agence)
+);
+
+CREATE TABLE personne(
+   nom VARCHAR(50),
+   prenom VARCHAR(50),
+   age INT NOT NULL,
+   PRIMARY KEY(nom)
+);
+
+CREATE TABLE etat_habitation(
+   ID_etat INT,
+   etat VARCHAR(50),
+   PRIMARY KEY(ID_etat)
+);
+
+CREATE TABLE Logement(
+   ID_logement INT,
+   adresse VARCHAR(50) NOT NULL,
+   type_propriete VARCHAR(50) NOT NULL,
+   nombre_pieces INT NOT NULL,
+   surface_habitable INT NOT NULL,
+   objectif_gestion VARCHAR(50) NOT NULL,
+   date_dispo DATE NOT NULL,
+   ville VARCHAR(50) NOT NULL,
+   est_dispo bool NOT NULL,
+   prix INT NOT NULL,
+   ID_etat INT NOT NULL,
+   nom_proprio VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_logement),
+   FOREIGN KEY(ID_etat) REFERENCES etat_habitation(ID_etat),
+   FOREIGN KEY(nom_proprio) REFERENCES Personne(nom)
+);
+
+CREATE TABLE Garage(
+   ID_garage INT,
+   nom VARCHAR(50) NOT NULL,
+   surface INT NOT NULL,
+   ID_logement INT NOT NULL,
+   PRIMARY KEY(ID_garage),
+   FOREIGN KEY(ID_logement) REFERENCES Logement(ID_logement)
+);
+
+CREATE TABLE transac(
+   ID_transaction INT,
+   commission INT NOT NULL,
+   type_vente VARCHAR(50) NOT NULL,
+   ID_agence INT NOT NULL,
+   ID_logement INT NOT NULL,
+   nom VARCHAR(50) NOT NULL,
+   PRIMARY KEY(ID_transaction),
+   FOREIGN KEY(ID_agence) REFERENCES Agence(ID_agence),
+   FOREIGN KEY(ID_logement) REFERENCES Logement(ID_logement),
+   FOREIGN KEY(nom) REFERENCES personne(nom)
+);
+
+CREATE TABLE Visite(
+   Id_Visite INT,
+   date_visite DATE NOT NULL,
+   nom_client VARCHAR(50),
+   ID_logement INT NOT NULL,
+   ID_agence INT NOT NULL,
+   PRIMARY KEY(Id_Visite),
+   FOREIGN KEY(nom_client) REFERENCES Personne(nom),
+   FOREIGN KEY(ID_logement) REFERENCES Logement(ID_logement),
+   FOREIGN KEY(ID_agence) REFERENCES Agence(ID_agence)
+);
